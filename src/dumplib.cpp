a7c1e15dfefa3ca56a160c9c162f4146deeaefee
Mark Harmstone <mark@harmstone.com>
2023-09-29 02:24:47 +0100

    add print_symbols

--- src/dumplib.cpp
+++ src/dumplib.cpp
@@ -2,6 +2,7 @@
 #include <bfd.h>
 #include <memory>
 #include <iostream>
+#include <format>
 
 using namespace std;
 
@@ -15,6 +16,41 @@ public:
 };
 
 using bfd_ptr = unique_ptr<bfd*, bfd_closer>;
+
+static void print_symbols(bfd* b) {
+    uint8_t* ptr;
+    unsigned int size;
+
+    auto count = bfd_read_minisymbols(b, 0, (void**)&ptr, &size);
+
+    cout << "---" << endl;
+    // FIXME - print archive filename
+
+    if (count <= 0)
+        return;
+
+    auto store = bfd_make_empty_symbol(b);
+    if (!store)
+        throw runtime_error("bfd_make_empty_symbol failed");
+
+    unique_ptr<uint8_t> v{ptr};
+
+    for (unsigned int i = 0; i < count; i++) {
+        symbol_info info;
+
+        auto sym = bfd_minisymbol_to_symbol(b, false, ptr, store);
+        if (!sym)
+            throw runtime_error("bfd_minisymbol_to_symbol failed");
+
+        bfd_get_symbol_info(b, sym, &info);
+
+        cout << format("{} {}\n", info.type, info.name);
+
+        // FIXME - only T (not .text)
+
+        ptr += size;
+    }
+}
 
 static void do_file(const char* fn) {
     bfd_ptr b{bfd_openr(fn, nullptr)};
@@ -40,7 +76,7 @@ static void do_file(const char* fn) {
         if (!bfd_check_format_matches(ar.get(), bfd_object, nullptr))
             throw runtime_error("file in archive was not an object");
 
-        // FIXME - print symbols
+        print_symbols(ar.get());
     }
 }
 
@@ -49,7 +85,7 @@ int main(int argc, char** argv) {
         try {
             do_file(argv[i]);
         } catch (const exception& e) {
-            cerr << "Failed to parse " << argv[i] << ": " << e.what() << endl;
+            cerr << format("Failed to parse {}: {}\n", argv[i], e.what());
         }
     }
 
